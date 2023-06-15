751c1e449427d3fcb3bc6afc247fcc3ad56b9c76
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-06-15 15:09:41 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <footer className="bg-[#f8f8f8] py-8">
         <div className="flex-col sm:flex-row sm:flex justify-between text-center mx-auto w-full max-w-[1000px]">
-            <h3>Copyright © 2023 BUIDL.AE - All Rights Reserved.</h3>
+            <h3>Copyright © 2023 BUIDL - All Rights Reserved.</h3>
             <div className='flex gap-8 items-center  text-center justify-center sm:justify-end'>
                 <a href="https://twitter.com/Mzn_Rahi" target='_blank' rel="noreferrer" className='border-1 rounded-full social'> <BsTwitter/></a>
                 <a href="https://www.linkedin.com/in/mzn-rahi/" target='_blank' rel="noreferrer" className='border-1 rounded-full social'> <BsLinkedin/></a>
